1ce8ae0556b91f6325aa8fbd8928bb3a17d2d8c6
s.laugh <shaun.laughland@hrsdc-rhdcc.gc.ca>
2019-06-05 16:00:09 -0400

    add renders to preFooter adding the content and if comparisons works on #19

--- src/wet-gcweb-components/PreFooterContentComponent.ts
+++ src/wet-gcweb-components/PreFooterContentComponent.ts
@@ -4,12 +4,12 @@ import * as CDTSConstants from '../CDTS/CDTSConstants';
 
 
 export default class PreFooterContentComponent extends CDTSBaseComponent {
-    
+
     constructor(targetElementId, data) {
         super(targetElementId, data);
         this.HasDataForRequiredProperties(data);
     }
-    
+
     HasDataForRequiredProperties(data) {
         var requiredProperties = [
             "pagedetails",
@@ -23,61 +23,82 @@ export default class PreFooterContentComponent extends CDTSBaseComponent {
         });
     }
 
-    generateHtml() : string {
+    generateHtml(): string {
         var html = "";
 
-        if (this.data.pagedetails) html += this.generatePageDetails();
+        html += this.generatePageDetails();
 
         return html;
     }
 
-    private generatePageDetails() : string {
+    private generatePageDetails(): string {
         var html = "";
 
-        html += `<div class="pagedetails"><div class="row">`
-        if (this.data.showFeedback != false) html += this.generateFeedBack();
-        if (this.data.showShare != false) html += this.generateShowShare();
-        html += `</div><dl id="wb-dtmd">`        
-        if (this.data.dateModified) html += this.generateDateModified();
-        if (this.data.screenIdentifier) html += this.generateScreenIdentifier();
-        if (this.data.versionIdentifier) html += this.generateVersionIdentifier();
-        html += `</dl></div>`
+        if (this.data.pagedetails) {
+            html += `<div class="pagedetails"><div class="row">`
+            html += this.generateFeedBack();
+            html += this.generateShowShare();
+            html += `</div><dl id="wb-dtmd">`
+            html += this.generateDateModified();
+            html += this.generateScreenIdentifier();
+            html += this.generateVersionIdentifier();
+            html += `</dl></div>`
+        }
 
         return html;
     }
 
-    private generateFeedBack() : string {
+    private generateFeedBack(): string {
         var html = "";
 
-        html += `<div class="col-sm-6 col-md-5 col-lg-4">
+        if (this.data.showFeedback != false) {
+            html += `<div class="col-sm-6 col-md-5 col-lg-4">
                     <a href="${this.data.showFeedback != true ? this.data.showFeedback : "https://www.canada.ca/en/report-problem.html"}" class="btn btn-default text-center">Report a problem on this page</a>
                 </div>`
+        }
 
         return html;
     }
 
-    private generateShowShare() : string {
+    private generateShowShare(): string {
         var html = "";
 
-        html += `<div class="wb-share col-sm-4 col-md-3 ${this.data.showFeedback == false ? "col-sm-offset-8 col-md-offset-9" : "col-sm-offset-2 col-md-offset-4 col-lg-offset-5"}" data-wb-share='{"lnkClass": "btn btn-default btn-block"}'></div>`
+        if (this.data.showShare != false) {
+            html += `<div class="wb-share col-sm-4 col-md-3 ${this.data.showFeedback == false ? "col-sm-offset-8 col-md-offset-9" : "col-sm-offset-2 col-md-offset-4 col-lg-offset-5"}" data-wb-share='{"lnkClass": "btn btn-default btn-block"}'></div>`
+        }
 
         return html;
     }
 
-    private generateDateModified() : string {
+    private generateDateModified(): string {
         var html = "";
+
+        if (this.data.dateModified) {
+            html += `<dt>Date modified:&#32;</dt>
+                <dd><time property="dateModified">${ this.data.dateModified}</time></dd>`
+        }
 
         return html;
     }
 
-    private generateScreenIdentifier() : string {
+    private generateScreenIdentifier(): string {
         var html = "";
+
+        if (this.data.screenIdentifier) {
+            html += `<dt>Screen Identifier:&#32;</dt>
+                <dd property="identifier">${ this.data.screenIdentifier}</dd>`
+        }
 
         return html;
     }
-    
-    private generateVersionIdentifier() : string {
+
+    private generateVersionIdentifier(): string {
         var html = "";
+
+        if (this.data.versionIdentifier) {
+            html += `<dt>Version:&#32;</dt>
+                <dd property="version">${ this.data.versionIdentifier}</dd>`
+        }
 
         return html;
     }
