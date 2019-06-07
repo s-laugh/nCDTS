6fbd105e61124a2c523800aac8350f3a7f319dc0
s.laugh <shaun.laughland@hrsdc-rhdcc.gc.ca>
2019-06-07 10:50:18 -0400

    fix linting error son preFooter

--- src/wet-gcweb-components/PreFooterContentComponent.ts
+++ src/wet-gcweb-components/PreFooterContentComponent.ts
@@ -1,30 +1,30 @@
-import CDTSBaseComponent from '../CDTS/CDTSBaseComponent';
-import * as CDTSConstants from '../CDTS/CDTSConstants';
+import CDTSBaseComponent from "../CDTS/CDTSBaseComponent";
+import * as CDTSConstants from "../CDTS/CDTSConstants";
 // import CDTSCommonHtmlGenerators from '../CDTS/CDTSCommonHtmlGenerators';
 
 
 export default class PreFooterContentComponent extends CDTSBaseComponent {
 
-    constructor(targetElementId, data) {
+    constructor(targetElementId : string, data : any) {
         super(targetElementId, data);
-        this.HasDataForRequiredProperties(data);
+        this.hasDataForRequiredProperties(data);
     }
 
-    HasDataForRequiredProperties(data) {
-        var requiredProperties = [
+    private hasDataForRequiredProperties(data : any): void {
+        let requiredProperties : string[] = [
             "pagedetails",
             "showFeedback",
             "showShare"];
-        var keys = Object.keys(this.data);
+        let keys : string[] = Object.keys(data);
         requiredProperties.forEach((required) => {
-            if (keys.includes(required) == false) {
+            if (keys.includes(required) === false) {
                 throw CDTSConstants.CDTS_INSTRUCTIONS_NOT_VALID;
             }
         });
     }
 
-    generateHtml(): string {
-        var html = "";
+    public generateHtml(): string {
+        let html : string = "";
 
         html += this.generatePageDetails();
 
@@ -32,72 +32,82 @@ export default class PreFooterContentComponent extends CDTSBaseComponent {
     }
 
     private generatePageDetails(): string {
-        var html = "";
+        let html : string = "";
 
         if (this.data.pagedetails) {
-            html += `<div class="pagedetails"><div class="row">`
+            html += `<div class="pagedetails"><div class="row">`;
             html += this.generateFeedBack();
             html += this.generateShowShare();
-            html += `</div><dl id="wb-dtmd">`
+            html += `</div><dl id="wb-dtmd">`;
             html += this.generateDateModified();
             html += this.generateScreenIdentifier();
             html += this.generateVersionIdentifier();
-            html += `</dl></div>`
+            html += `</dl></div>`;
         }
 
         return html;
     }
 
     private generateFeedBack(): string {
-        var html = "";
+        let html : string = "";
 
-        if (this.data.showFeedback != false) {
+        if (this.data.showFeedback !== false) {
             html += `<div class="col-sm-6 col-md-5 col-lg-4">
-                    <a href="${this.data.showFeedback != true ? this.data.showFeedback : "https://www.canada.ca/en/report-problem.html"}" class="btn btn-default text-center">Report a problem on this page</a>
-                </div>`
+                    <a href="
+                        ${ this.data.showFeedback !== true ?
+                            this.data.showFeedback :
+                            "https://www.canada.ca/en/report-problem.html"
+                        }
+                    " class="btn btn-default text-center">Report a problem on this page</a>
+                    </div>`;
         }
 
         return html;
     }
 
     private generateShowShare(): string {
-        var html = "";
+        let html : string = "";
 
-        if (this.data.showShare != false) {
-            html += `<div class="wb-share col-sm-4 col-md-3 ${this.data.showFeedback == false ? "col-sm-offset-8 col-md-offset-9" : "col-sm-offset-2 col-md-offset-4 col-lg-offset-5"}" data-wb-share='{"lnkClass": "btn btn-default btn-block"}'></div>`
+        if (this.data.showShare !== false) {
+            html += `<div class="wb-share col-sm-4 col-md-3
+                    ${ this.data.showFeedback === false ?
+                        "col-sm-offset-8 col-md-offset-9" :
+                        "col-sm-offset-2 col-md-offset-4 col-lg-offset-5"
+                    }
+                    " data-wb-share='{"lnkClass": "btn btn-default btn-block"}'></div>`;
         }
 
         return html;
     }
 
     private generateDateModified(): string {
-        var html = "";
+        var html : string = "";
 
         if (this.data.dateModified) {
             html += `<dt>Date modified:&#32;</dt>
-                <dd><time property="dateModified">${ this.data.dateModified}</time></dd>`
+                    <dd><time property="dateModified">${ this.data.dateModified }</time></dd>`;
         }
 
         return html;
     }
 
     private generateScreenIdentifier(): string {
-        var html = "";
+        var html : string = "";
 
         if (this.data.screenIdentifier) {
             html += `<dt>Screen Identifier:&#32;</dt>
-                <dd property="identifier">${ this.data.screenIdentifier}</dd>`
+                <dd property="identifier">${ this.data.screenIdentifier }</dd>`;
         }
 
         return html;
     }
 
     private generateVersionIdentifier(): string {
-        var html = "";
+        var html : string = "";
 
         if (this.data.versionIdentifier) {
             html += `<dt>Version:&#32;</dt>
-                <dd property="version">${ this.data.versionIdentifier}</dd>`
+                <dd property="version">${ this.data.versionIdentifier}</dd>`;
         }
 
         return html;
