d34ed872cf9ea179b94a0a16f85ab86a06907601
s.laugh <shaun.laughland@hrsdc-rhdcc.gc.ca>
2019-06-07 13:13:36 -0400

    adding preFooter to register components

--- src/wet-gcweb-components/wetGcweb.ts
+++ src/wet-gcweb-components/wetGcweb.ts
@@ -1,13 +1,15 @@
 import SplashContentComponent from "./SplashContentComponent";
 import TopContentComponent from "./TopContentComponent";
+import PreFooterContentComponent from "./PreFooterContentComponent";
 
-const components = {
+const components : any = {
   SplashContentComponent,
-  TopContentComponent
+  TopContentComponent,
+  PreFooterContentComponent
 };
 
 export default class ModuleComponents {
-  constructor(componentname, targetelement, opts) {
-    return new components[componentname](targetelement, opts);
+  constructor(componentName : string, targetelement : string, opts : any) {
+    return new components[componentName](targetelement, opts);
   }
 }
