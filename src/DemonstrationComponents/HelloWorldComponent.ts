7a08a337b6ac25b249a11c26f09ee4b852be5458
S Grabinski <stan.grabinski@hrsdc-rhdcc.gc.ca>
2019-06-12 22:30:21 -0400

    Last of the TSLint clean up for now.

--- src/DemonstrationComponents/HelloWorldComponent.ts
+++ src/DemonstrationComponents/HelloWorldComponent.ts
@@ -1,17 +1,17 @@
-import CDTSBaseComponent from '../CDTS/CDTSBaseComponent';
+import CDTSBaseComponent from "../CDTS/CDTSBaseComponent";
 
 export default class HelloWorldComponent extends CDTSBaseComponent {
 
-    generateHtml() {
+    constructor(targetElementId, data) {
+        super(targetElementId, data);
+    }
+
+    public generateHtml() {
         if (this.data) {
             if (this.data.name) {
                 return `<p>Hello ${this.data.name}!</p>`;
             }
         }
         return "<p>Hello World!</p>";
-    }
-
-    constructor(targetElementId, data) {
-        super(targetElementId, data);
     }
 }
